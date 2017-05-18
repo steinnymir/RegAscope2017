f1922b8480d3079e62137c25ad0c4f6a5012fde8
steinnymir <sagustss@uni-mainz.de>
2017-05-18 18:57:00 +0200

    new name interpreter
    called getMetadataFromName()

--- functionlibrary/genericfunctions.py
+++ functionlibrary/genericfunctions.py
@@ -106,79 +106,109 @@ def sym(letter):
             return(inv_map[key])
 
 
-
-
-
-
-
 #%% naming
 
-def name_to_info(file):
-    """
-    Interprets measurement parameters out of the file name
 
-    it understands the following:
-    'Pump Power' : 'pu','pump',
-    'Probe Power': 'pr','probe',
-    'Temperature': 't','temp',
-    'Destruction Power': 'd','dest','destr'
-
-    """
-    #lowercase file name without .* extension
-    FileName = ('.').join(os.path.basename(file).split('.')[:-1])
-    filenamex = FileName.lower()
-    filename = filenamex.replace(',','.')
-    #print(filename)
-
-    #errStr = FileName + ' contains no info about: '
-
-    #define parameter set
-    parameterDict = {'date' : file_creation_date(file),
-                     'material'   : '',
-                     'pump_power' : 0,
-                     'probe_power': 0,
-                     'temperature': 0,
-                     'destruction_power': 0,
-                     'other': '',
-                     }
-    #set of possible indicators and corresponding key
-    parInd = {'pump_power' : ['pu','pump'],
-              'probe_power': ['pr','probe'],
-              'temperature': ['t','temp'],
-              'destruction_power': ['d','dest','destr'],
-              }
-    # Iterate over possible indicators and save value to corresponding key in
-    parameterIndicatorTrue = []
-    for key, indicators in parInd.items():
-        for string in indicators:
-            if string in filename:
-                parameterIndicatorTrue.append(string)
-                # Partition string around parameter delimiter and pick the
-                # first following float number
-                try:
-                    value = float(re.findall(r"\d+\.\d*",
-                                         filename.partition(string)[2])[0])
-                    parameterDict[key] = value
-                except IndexError:
-                    pass
-                #append in Dictionary of parameters
-
-
-    pos = 100
-    for string in parameterIndicatorTrue:
-       x = filename.find(string)
-       if x < pos:
-           pos=x
-
-    parameterDict['material'] = FileName[0:pos]
-
-    return(parameterDict)
 
 class NameString(object):
     """ gets names of variables from whithin a class"""
     def __init__(self,*values):
         for v in values:
             self.__dict__[v] = v
+
+
+
+def getMetadataFromName(filepath):
+    ''' interprets name of file and returns a dictionary with all contained metadata
+    it requires a name with elements separated by ['-','_',',',' '].
+    example: RuCl3_pu_15mW_pr_5mW_t_4.5K.mat
+             RuCl3-pu-15mW-pr-5mW-t-4.5K.mat
+
+    replacement of old name_to_info'''
+    # transform to more usable string form
+
+    metadataDict = {}
+    metadataDict['date'] = file_creation_date(filepath) # get file creation date
+
+    FileName = ('.').join(os.path.basename(filepath).split('.')[:-1])
+    filename = FileName.lower().replace(',','.')
+
+    #identify most recurring possible separator from separators list in filename
+    separators = ['-','_',',',' ']
+    sepCount = {}
+    for item in separators:
+        sepCount[item] = filename.count(item)
+
+    inverse = [(value, key) for key, value in sepCount.items()]
+    sep = max(inverse)[1]
+    #generate a list of elements as separated by sep
+
+
+    metadata_names = {'pump_power' : ['pu','pump'],
+                      'probe_power': ['pr','probe'],
+                      'destruction_power': ['d','dest','destr'],
+                      'temperature': ['t','temp'],
+                      'pump_polarization': ['pupol'],
+                      'probe_polarization': ['prpol'],
+                      'destruction_polarization': ['dpol'],
+                      'sample_orientation': ['sor'],
+              }
+#    metadata_names_inv =  [(value, key) for key, value in metadata_names.items()]
+#    print(metadata_names_inv)
+    parameter_list = []
+
+    if sepCount[sep] > 1: # use separator based interpreter
+        filename_list = filename.split(sep)
+        for key, item in metadata_names.items():
+            for parameter in item:
+
+                try:
+                    # look for an identifier as defined in metadata_names and
+                    # if there, get the value in the next item, that should be
+                    # the value corresponding to such parameter
+                    parameter_index = filename_list.index(parameter)
+
+                    value = re.findall("\d+\.\d+", filename_list[parameter_index + 1])
+                    parameter_list.append(parameter)
+                    # save retrieved parameter value
+                    metadataDict[key] = float(value[0])
+                    filename_list.pop(parameter_index) # twice because first removes
+                    filename_list.pop(parameter_index) # string, second the value
+                except :
+                    pass
+        metadataDict['material'] = filename_list.pop(0)
+        metadataDict['other'] = filename_list
+
+    else: # use 'parameter name search' based interpreter
+        parameter_list = []
+        for key, item in metadata_names.items():
+            for parameter in item:
+
+                if parameter in filename:
+                    parameter_list.append(parameter)
+                    try:
+                        # Partition string around parameter delimiter and pick the
+                        # first following float number
+                        parameter_list.append(parameter)
+                        value = float(re.findall(r"\d+\.\d*",
+                                             filename.partition(parameter)[2])[0])
+                        metadataDict[key] = value
+                    except IndexError:
+                        pass
+        pos = 100
+        for string in parameter_list:
+            x = filename.find(string)
+            if x < pos:
+                pos=x
+
+        metadataDict['material'] = FileName[0:pos]
+    print(filename)
+
+    return(metadataDict)
+
+
+
+
 
 #%% Transformations
 
